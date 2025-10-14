6b1e32fbc93229d0fdf3f1055e4149b218e1b492
RylinWolf <linexwolf@icloud.com>
2025-10-14 13:11:31 +0800

    feat: 新增自定义异常工具

---
+++ src/main/java/com/wolfhouse/yuaicodemother/exception/BusinessException.java
@@ -0,0 +1,23 @@
+package com.wolfhouse.yuaicodemother.exception;
+
+/**
+ * @author linexsong
+ */
+public class BusinessException extends RuntimeException {
+    private final int code;
+
+    public BusinessException(String message, int code) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+}
