642759e30883342e551e07313fd073e0a2ddd393
RylinWolf <linexwolf@icloud.com>
2025-10-21 13:13:27 +0800

    feat: 可视化修改（前端）

--- src/main/java/com/wolfhouse/yuaicodemother/ai/AiCodeGeneratorServiceFactory.java
+++ src/main/java/com/wolfhouse/yuaicodemother/ai/AiCodeGeneratorServiceFactory.java
@@ -89,7 +89,7 @@ public class AiCodeGeneratorServiceFactory {
             .builder()
             .id(appId)
             .chatMemoryStore(redisChatMemoryStore)
-            .maxMessages(30)
+            .maxMessages(100)
             .build();
         // 从数据库中加载对话历史到记忆中
         chatHistoryService.loadChatHistoryToMemory(appId, chatMemory, 20);
