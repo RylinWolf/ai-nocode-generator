b138c784dc27248ecb3962a34c855d21652f6796
RylinWolf <linexwolf@icloud.com>
2025-10-23 16:47:06 +0800

    feat: 后端 AI 工作流高级特性开发 （并发） 基于 LangGraph4j Parallel Branch 实现并发

--- src/main/java/com/wolfhouse/yuaicodemother/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/wolfhouse/yuaicodemother/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.wolfhouse.yuaicodemother.langgraph4j.state;
 
+import com.wolfhouse.yuaicodemother.langgraph4j.model.ImageCollectionPlan;
 import com.wolfhouse.yuaicodemother.langgraph4j.model.ImageResource;
 import com.wolfhouse.yuaicodemother.langgraph4j.model.QualityResult;
 import com.wolfhouse.yuaicodemother.model.enums.CodeGenTypeEnum;
@@ -41,10 +42,22 @@ public class WorkflowContext implements Serializable {
      * 图片资源字符串
      */
     private String imageListStr;
-    /**
-     * 图片资源列表
-     */
     private List<ImageResource> imageList;
+    
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
+
     /**
      * 增强后的提示词
      */
